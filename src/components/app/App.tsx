11b47c093ab56f60a71aa15b639975a1fe6dfe98
Anna Ivanchenko <bubachka1990@gmail.com>
2026-04-13 23:24:44 +0300

    create 02-react-cafe homework

---
+++ src/components/app/App.tsx
@@ -0,0 +1,56 @@
+import CafeInfo from "../cafe_info/CafeInfo";
+import VoteOptions from "../vote_options/VoteOptions";
+import VoteStats from "../vote_stats/VoteStats";
+import Notification from "../notification/Notification";
+import css from "./App.module.css";
+import { useState } from "react";
+import type { Votes, VoteType } from "../../types/votes";
+
+const initialVotes: Votes = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+}
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>(initialVotes);
+  
+  const handleVote = (voteType: VoteType) => {
+    setVotes((prevVotes) => ({
+      ...prevVotes,
+      [voteType]: prevVotes[voteType] + 1,
+    }));
+  };
+  const resetVotes = () => {
+    setVotes(initialVotes);
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+const positiveRate = totalVotes
+  ? Math.round((votes.good / totalVotes) * 100)
+  : 0;
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+          <Notification />
+      )}
+    </div>
+  );
+}
+
+
+
