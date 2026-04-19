980ac37c3d0f408754490c3731285bd831e3e711
Anna Ivanchenko <bubachka1990@gmail.com>
2026-04-19 13:43:50 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@
 // )
 import { StrictMode } from 'react';
 import { createRoot } from "react-dom/client";
-import App from "./components/app/App";
+import App from "./components/App/App";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
